3ab5ce66921d4defcedba1c53a78a17e5aa47ef4
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-09-02 20:14:58 +0300

    Modify background style from app component to body element

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
